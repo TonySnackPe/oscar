3e7147280e4134878da14094caf532ebfc6d3a69
TONY <130157657+TonySnackPe@users.noreply.github.com>
2024-09-20 15:22:22 -0500

    Add files via upload

---
+++ index.php
@@ -0,0 +1,42 @@
+<title>GN Flash Player AacPlus V2 - Demo 03</title><style type="text/css">
+<!--
+body {
+	background-color: #eeeeee;
+}
+#apDiv1 {
+	position:absolute;
+	width:200px;
+	height:115px;
+	z-index:1;
+	left: 720px;
+	top: 374px;
+}
+#apDiv2 {
+	position:absolute;
+	width:200px;
+	height:115px;
+	z-index:1;
+	left: -433px;
+	top: 21px;
+}
+-->
+</style><object classid="clsid:D27CDB6E-AE6D-11cf-96B8-444553540000" codebase="http://download.macromedia.com/pub/shockwave/cabs/flash/swflash.cab#version=7,0,19,0" width="292" height="223">
+        <param name="movie" value="gnplayer3.swf">
+        <param name="quality" value="high">
+        <embed src="gnplayer3.swf" quality="high" pluginspage="http://www.macromedia.com/go/getflashplayer" type="application/x-shockwave-flash" width="292" height="223"></embed>
+        </object>
+<div id="apDiv1"><script type="text/javascript" src="http://www.netandino.com/radios/swfobject.js"></script><title>Theme kleur</title>
+<div id="GN Player"><a href="http://get.adobe.com/flashplayer/">Reproductor Flash Player</a><a href="http://www.macromedia.com/go/getflashplayer"></a> paraa ver a este reproductor</div>
+<script type="text/javascript">
+var gn = new SWFObject("http://www.netandino.com/radios/player.swf","ply","275","55","9","#FFFFFF");
+gn.addParam("allowfullscreen","true");
+gn.addParam("allowscripaccess","always");
+gn.addParam("flashvars","file=http://listen.radionomy.com/talentodbarriosa&type=mp3&skin=http://www.netandino.com/radios/theme.swf&volume=0&autostart=true");
+gn.write("GN Player");
+</script>
+<div id="apDiv2"><br><script language="JavaScript" src="http://www.counter160.com/js.js?img=15"></script><br><a href="http://www.000webhost.com"><img src="http://www.counter160.com/images/15/left.png" alt="Free web hosting" border="0" align="texttop"></a><a href="http://www.hosting24.com"><img alt="Web hosting" src="http://www.counter160.com/images/15/right.png" border="0" align="texttop"></a></div>
+</div>
+
+<!-- Hosting24 Analytics Code -->
+<script type="text/javascript" src="http://stats.hosting24.com/count.php"></script>
+<!-- End Of Analytics Code -->
